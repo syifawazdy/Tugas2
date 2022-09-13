d29ae8d2b00eb405580cc374038adfbbc904abe8
syifawazdy <syifa.mumtaz@ui.ac.id>
2022-09-13 14:45:04 +0700

    Tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Syifa',
+    'id': '2106701066',
+}
+    return render(request, "katalog.html", context)
+
+    

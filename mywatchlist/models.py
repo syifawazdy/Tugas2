fae1f83b634a70cda2f1ad3e33611859ed29132b
syifawazdy <syifa.mumtaz@ui.ac.id>
2022-09-20 23:56:42 +0700

    Second Progress

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class MyWatchlist(models.Model):
+class MyWatchList(models.Model):
     watched = models.BooleanField()
     title = models.TextField()
     rating = models.IntegerField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
+
+def show_mywatchlist(request):
+    return render(request, "mywatchlist.html", context)
+
+mywatchlist = MyWatchList.objects.all()
+context = {
+    'list_barang': mywatchlist,
+    'nama': 'Syifa',
+    'id': '2106701066',
+}
 
 # Create your views here.
+def show_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")

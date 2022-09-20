ccc03a9bdc9d66ff8d1e72e2e5fd9ee25b14b8b8
syifawazdy <syifa.mumtaz@ui.ac.id>
2022-09-20 14:42:20 +0700

    First Progress

---
+++ mywatchlist/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class MyWatchlist(models.Model):
+    watched = models.BooleanField()
+    title = models.TextField()
+    rating = models.IntegerField()
+    release_date = models.DateField()
+    review = models.TextField()

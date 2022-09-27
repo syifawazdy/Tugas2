fe06387c01a3b8359d8eeddffc5a34124694f226
syifawazdy <syifa.mumtaz@ui.ac.id>
2022-09-27 17:22:50 +0700

    Tugas 4

--- todolist/models.py
+++ todolist/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateField(default=timezone.now)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
